5b74d98d826de44c5248403fc9f123dfefe92812
Guilherme Marinho <guilherme.marinho@levva.com>
2023-02-01 09:43:56 -0300

    Automacao restAssured

---
+++ src/test/java/dataFactory/ProdutoDataFactory.java
@@ -0,0 +1,32 @@
+package dataFactory;
+
+import pojo.ComponentePojo;
+import pojo.ProdutoPojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoDataFactory {
+    public static ProdutoPojo criarProdutoComumComOValorIgualA(double valor){
+        ProdutoPojo produto = new ProdutoPojo();
+        produto.setProdutoNome("Play Station 8");
+        produto.setProdutoValor(valor);
+        List<String> cores = new ArrayList<>();
+        cores.add("preto");
+        cores.add("branco");
+
+        produto.setProdutoCores(cores);
+        produto.setProdutoUrlMock("");
+
+        List<ComponentePojo> componentes = new ArrayList<>();
+
+        ComponentePojo componente = new ComponentePojo();
+        componente.setComponenteNome("Controle");
+        componente.setComoponenteQuantidade(1);
+
+        produto.setComponentes(componentes);
+
+        return produto;
+    }
+
+}
---
+++ src/test/java/pojo/ComponentePojo.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class ComponentePojo {
+    private String componenteNome;
+    private int comoponenteQuantidade;
+
+    public String getComponenteNome() {
+        return componenteNome;
+    }
+
+    public void setComponenteNome(String componenteNome) {
+        this.componenteNome = componenteNome;
+    }
+
+    public int getComoponenteQuantidade() {
+        return comoponenteQuantidade;
+    }
+
+    public void setComoponenteQuantidade(int comoponenteQuantidade) {
+        this.comoponenteQuantidade = comoponenteQuantidade;
+    }
+}
